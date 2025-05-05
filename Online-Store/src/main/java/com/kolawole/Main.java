03d3e4f93a786f1f3e07c92127fe92e123144731
olumide-AI <olumidekolawole26@gmail.com>
2025-05-05 06:01:48 -0700

    final changes made

--- Online-Store/src/main/java/com/kolawole/Main.java
+++ Online-Store/src/main/java/com/kolawole/Main.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Main {
@@ -11,7 +15,7 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     //Class File
     private static final String FILE_PATH = "products.csv";
-    //Global variable cart
+    //Global variable cart matching SKU to the quanitiy of prosuct needed
     private static final Map<String, Integer> cart = new HashMap<>();
     public static void main(String[] args) {
         //Home Screen
@@ -29,6 +33,7 @@ public class Main {
                     break;
                 case "C":
                     //method to display user cart
+                    displayCartScreen();
                     break;
                 case "E":
                     System.out.println("Thank You Goodbye");
@@ -86,9 +91,11 @@ public class Main {
             switch (userInputCart){
                 case "C":
                     //method to checkout product
+                    cartCheckout();
                     break;
                 case "A":
                     //method to remove products from cart
+                    removeProductFromCart();
                     break;
                 case "X":
                     //method to go back to the Home Screen
@@ -131,14 +138,26 @@ public class Main {
     public static void addProductToCart(){
         //Ask user for a SKU
         System.out.println("To add an item to your cart, please provide item SKU number");
-        String userSkuInput = scanner.nextLine();
+        String userSkuInput = scanner.nextLine().toUpperCase().trim();
         //Load the products list
         List<Product> productList = loadProducts();
         //Search for matching products
         boolean flag = false;
         for (Product product : productList){
             if(product.getSku().equalsIgnoreCase(userSkuInput)){
-                cart.add(product);
+                //Check of product is already in cart
+                if(cart.containsKey(userSkuInput)){
+                    //Already in cart - get current quantity
+                    int currentQuantity = cart.get(userSkuInput);
+                    //increase by 1
+                    int newQuantity = currentQuantity + 1;
+                    //Update cart
+                    cart.put(userSkuInput, newQuantity);
+                }
+                else {
+                    //Not in cart - add with quantity 1
+                    cart.put(userSkuInput, 1);
+                }
                 System.out.println(product.getProductName() + " has been added to you cart.");
                 flag = true; //found the product
                 break;
@@ -174,6 +193,7 @@ public class Main {
                 case "D":
                     //Filter by department
                     filterByDepartment();
+                    break;
                 case "X":
                     flag = false;
                     break;
@@ -262,7 +282,107 @@ public class Main {
 
     }
 
+    public static void removeProductFromCart(){
+        //Ask user what product to remove
+        System.out.println("Enter the SKU value of the product you would like to remove from your cart");
+        String userSkuInput = scanner.nextLine().toUpperCase().trim();
+        //Check if the product is in the cart
+        if(cart.containsKey(userSkuInput)) {
+            //Get the current quantity. This the value of the map
+            int currentQuantity = cart.get(userSkuInput);
+            //Decrease or remove quantity
+            if (currentQuantity > 1) {
+                int newQuantity = currentQuantity - 1;
+                //update cart with the new quantity
+                cart.put(userSkuInput, newQuantity);
+                System.out.println("Removed one of item SKU " + userSkuInput + ". " + newQuantity + " remaining.");
+            } else {
+                //If only one remains, remove the whole product
+                cart.remove(userSkuInput);
+                System.out.println("Removed one of item SKU " + userSkuInput + " from your cart.");
+            }
+        }
+        else{
+            //The product isnot in the cart ata ll
+            System.out.println("Product not in the cart ");
+        }
+
+    }
+
+    //Displaying cart content
+    public static Product findProductBySku(String sku) {
+        for (Product product : loadProducts()) {
+            if (product.getSku().equalsIgnoreCase(sku)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+
+    public static void cartCheckout(){
+        if(cart.isEmpty()){
+            throw new IllegalArgumentException("Your cart is empty");
+        }
+        double total = 0;
+        //Calculate total
+        for (Map.Entry<String, Integer> entry : cart.entrySet()){
+            String sku = entry.getKey();
+            int quantity = entry.getValue();
+            Product product = findProductBySku(sku);
+            //If product is found, multiply price by quantitu
+            if (product != null){
+                total += product.getPrice() * quantity;
+            }
+        }
+        //Prompt for payment
+        double payment = 0;
+        boolean validPayment = false;
+        while(!validPayment){
+            System.out.printf("Your total is $%.2f. Enter payment amount: ", total);
+            try{
+                payment = Double.parseDouble(scanner.nextLine().trim());
+                if(payment < total){
+                    System.out.println("Not enough money. Please enter at least $" + total);
+                }
+                else {
+                    validPayment = true;
+                }
+            }
+            catch (NumberFormatException e){
+                System.out.println("Enter a valid number");
+            }
+        }
+
+        //Calcultae and display change
+        double change = payment - total;
+        LocalDateTime orderDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy h:mm a");
+        System.out.println(" Sales Receipt ");
+        System.out.println("Order Date: " + orderDateTime.format(formatter));
 
 
 
-}
+        //Loop throw items in the cart
+        for (Map.Entry<String, Integer> entry : cart.entrySet()) {
+            String sku = entry.getKey();
+            int quantity = entry.getValue();
+            Product product = findProductBySku(sku);
+            if (product != null){
+                double subtotal = product.getPrice() * quantity;
+                System.out.printf("%s | %s | $%.2f x %d = $%.2f\n", sku, product.getProductName(), product.getPrice(), quantity, subtotal);
+            }
+        }
+        System.out.printf("Total: $%.2f\n", total);
+        System.out.printf("Paid: $%.2f\n", payment);
+        System.out.printf("Change: $%.2f\n", change);
+
+        // Step 4: Clear cart
+        cart.clear();
+        System.out.println("Thank you for your purchase! Returning to Home Screen...\n");
+    }
+
+    }
+
+    
+
